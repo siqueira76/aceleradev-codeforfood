5af4b5e4bad564607edb4357376516e0807a8091
hdalmora <henrique_dalmora@hotmail.com>
2019-03-04 13:27:50 -0300

    Change ProductByEmporium endpoint name from produtos to products

--- src/main/java/com/codeforfood/mapfood/resources/ProductByEmporiumResources.java
+++ src/main/java/com/codeforfood/mapfood/resources/ProductByEmporiumResources.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/produtos")
+@RequestMapping(value = "/products")
 public class ProductByEmporiumResources {
 
     @Autowired
